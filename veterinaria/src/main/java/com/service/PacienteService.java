263f3b342f7b7aec3470ace54eb25c5f7bc6fd25
AlexDivs99 <a01367863@itesm.mx>
2021-11-02 21:37:12 -0600

    Avance 2
    stan loona

--- veterinaria/src/main/java/com/service/PacienteService.java
+++ veterinaria/src/main/java/com/service/PacienteService.java
@@ -1,5 +1,99 @@
 package com.service;
 
-public class PacienteService {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.model.Dueno;
+import com.model.Paciente;
+import com.utils.Conexion;
+
+public class PacienteService {
+	Paciente paciente;
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	
+	
+	public Paciente altaPaciente() throws SQLException
+	{
+		Conexion conexion = new Conexion();
+		Connection conn = conexion.establishConnection().getCon();
+		
+		String sql = "INSERT INTO paciente (nombre,categoria,edad) VALUES (?,?,?)";
+		
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, paciente.getNombre());
+		ps.setString(2, paciente.getCategoria());
+		ps.setInt(3, paciente.getEdad());
+		
+		ps.executeUpdate();
+		
+		conexion.closeConnection();
+		return paciente;
+	}
+	
+	public List<Paciente> consultarPacientes() throws SQLException
+	{
+		Conexion conexion = new Conexion();
+		Connection conn = conexion.establishConnection().getCon();
+		
+		String sql = "SELECT nombre, categoria, edad FROM producto";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		List<Paciente> pacientes=new ArrayList<Paciente>();
+		
+		while(rs.next()) {
+			Paciente paciente= new Paciente();
+		
+			paciente.setNombre(rs.getString(1));
+			paciente.setCategoria(rs.getString(2));
+			paciente.setEdad(rs.getInt(3));
+			
+			pacientes.add(paciente);
+			
+		}
+		
+		conexion.closeConnection();
+		return pacientes;
+	}	
+	
+	/*public Paciente entradaProducto() throws SQLException
+	{
+		Conexion conexion = new Conexion();
+		Connection conn = conexion.establishConnection().getCon();
+		
+		String sql = "UPDATE  producto SET existencia=? WHERE idProducto=?";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setInt(1, producto.getExistencia());
+		ps.setInt(2, producto.getIdProducto());
+		
+		ps.executeUpdate();
+		conexion.closeConnection();
+		return producto;
+	}
+	
+	public Paciente salidaProducto() throws SQLException
+	{
+		Conexion conexion = new Conexion();
+		Connection conn = conexion.establishConnection().getCon();
+		
+		String sql = "UPDATE producto SET existencia=? WHERE nombre=?";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setInt(1, producto.getExistencia());
+		ps.setString(2, producto.getNombre());
+		
+		ps.executeUpdate();
+		conexion.closeConnection();
+		return producto;
+	}*/
 }
--- veterinaria/src/main/webapp/WEB-INF/pacientes/altaPaciente.jsp
+++ veterinaria/src/main/webapp/WEB-INF/pacientes/altaPaciente.jsp
@@ -1,12 +1,56 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>    
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<meta charset="UTF-8">
+<title>Alta Paciente</title>
+	<link href="styles/validation.css" rel="stylesheet">
 </head>
 <body>
-
+<div class="container">
+	 <div class="row mb-5">
+    <%@include file="../menu.jsp" %><br>
+  	</div>
+  	
+	<s:if test="hasActionErrors()">
+		<s:actionerror/>
+	</s:if>
+	
+	<div class="row">
+	 	<div class="col">
+			<s:form action="altaPaciente" method="POST" id="formulario">
+			<div class="row form-group ">
+				<div class="col-sm-12 col-md-6 col-lg-3 control-group">
+				<label for=Nombre class="form-label">Nombre</label>
+				<s:textfield name="paciente.nombre" id="Nombre" cssClass="form-control" required="true"/>
+				<s:fielderror cssClass="text-danger" fieldName="paciente.nombre"/>
+				</div>
+				
+				<div class="col-sm-12 col-md-6 col-lg-3 control-group">
+				<label for=Categoria class="form-label">Categoria</label>
+				<s:textfield name="paciente.categoria" id="Nombre" cssClass="form-control" required="true"/>
+				<s:fielderror cssClass="text-danger" fieldName="paciente.categoria"/>
+				</div>
+				
+				<div class="col-sm-12 col-md-6 col-lg-3 control-group">
+				<label for=Edad class="form-label">Edad</label>
+				<s:textfield name="paciente.edad" id="Nombre" cssClass="form-control" required="true"/>
+				<s:fielderror cssClass="text-danger" fieldName="paciente.edad"/>
+				</div>
+			</div>
+				<div class="btn-group mt-2 col-sm-12 col-md-6 col-lg-4 " role="group" >
+					<s:submit value="Enviar" cssClass="btn btn-outline-primary"/>
+					<s:url var="regresar" action="consultarPaciente">
+					</s:url>
+					<s:a href="%{regresar}" cssClass="btn btn-outline-danger">Cancelar</s:a>
+				</div>
+	</s:form>
+	</div>
+</div>
+	<br>
+</div>
+	<script src="scripts/datosProducto.js"></script>	
 </body>
 </html>
